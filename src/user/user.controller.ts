d07abc29002d9aa7fbffb6aca2fc0b010d871c17
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-06-08 13:31:06 +0300

    :alien:  feat: Регистрация + валидация

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UserService } from './user.service'
 
@@ -7,12 +13,13 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto)
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll()
-  }
+  // @Get()
+  // findOne() {
+  //   return this.userService.findOne()
+  // }
 }
